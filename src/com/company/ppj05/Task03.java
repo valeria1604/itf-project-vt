b51e0d9ec4889e1a60aa52407f5f3234e359e8e6
Valeria <valtpocketbook@gmail.com>
2023-04-01 11:28:31 +0200

    ppj05 task03 variant1 implemented

--- src/com/company/ppj05/Task03.java
+++ src/com/company/ppj05/Task03.java
@@ -9,40 +9,34 @@ public class Task03 {
         int b = scanner.nextInt();
         int c = scanner.nextInt();
         System.out.println(a + " " + b + " " + c);
-        int min = 0;
-        int mid = 0;
-        int max = 0;
+        variant1(a, b, c);
+    }
 
-        for (int i = 0; i < 3; i++) {
-            if (i == 0) {
-                if (checkMin(a, b, c)) min = a;
-                else if (checkMax(a, b, c)) max = a;
-                else {
-                    mid = a;
-                }
-            }
-            if (i == 1) {
-                if (checkMin(b, a, c)) min = b;
-                else if (checkMax(b, a, c)) max = b;
-                else {
-                    mid = b;
-                }
-            }
-            if (i == 2) {
-                if (min == 0) min = c;
-                if (mid == 0) mid = c;
-                if (max == 0) max = c;
-            }
-        }
+    private static void variant1(int a, int b, int c) {
+        int min = findMin(a, b, c);
+        int max = findMax(a, b, c);
+        int mid = (a + b + c) - min - max;
         System.out.println(min + " " + mid + " " + max);
     }
 
-    private static boolean checkMin(int tmp, int x1, int x2) {
-        return (tmp <= x1 && tmp <= x2);
+    private static int findMin(int a, int b, int c) {
+        if (a <= b && a <= c) {
+            return a;
+        }
+        if (b <= c) {
+            return b;
+        }
+        return c;
     }
 
-    private static boolean checkMax(int tmp, int x1, int x2) {
-        return (tmp >= x1 && tmp >= x2);
+    private static int findMax(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        }
+        if (b >= c) {
+            return b;
+        }
+        return c;
     }
 }
 
