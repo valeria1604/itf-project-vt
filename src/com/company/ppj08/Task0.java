1327012df61f0243b2e1a2fe607bde3acdc0f29a
Valeria <valtpocketbook@gmail.com>
2023-04-05 20:54:06 +0200

    ppj08 task0 approved

--- src/com/company/ppj08/Task0.java
+++ src/com/company/ppj08/Task0.java
@@ -4,24 +4,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Task0 {
-    public static String[] figures = new String[]{"disc", "triangle", "square"};
+    public static final String[] FIGURES = new String[]{"disc", "triangle", "square"};
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String figure = scanner.nextLine();
         double d;
         if (figure.equals("rand")) {
-            int randomNumber = new Random().nextInt(figures.length + 1);
-            figure = figures[randomNumber];
-            d = Math.random();
+            Random random = new Random();
+            int randomNumber = random.nextInt(FIGURES.length);
+            figure = FIGURES[randomNumber];
+            d = random.nextInt(10);
         } else {
             d = scanner.nextInt();
         }
+
         System.out.println("Figure: " + figure);
         System.out.println("Number: " + d);
+
         switch (figure) {
             case "disc" -> System.out.println("Area disc: " + Math.PI * Math.pow(d, 2));
-            case "triangle" -> System.out.println("Area triangle: " + (Math.pow(d, 2) * Math.sqrt(3) / 4));
+            case "triangle" -> System.out.println("Area triangle: " + ((Math.pow(d, 2) * Math.sqrt(3)) / 4));
             case "square" -> System.out.println("Area square: " + Math.pow(d, 2));
             default -> throw new IllegalStateException("Unexpected value: " + figure);
         }
