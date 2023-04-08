a1dc4c699e0cd2212833b5bc950b2e8da2f86d89
Valeria <valtpocketbook@gmail.com>
2023-04-08 10:10:05 +0200

    ppj08 task3 changed

--- src/com/company/ppj08/Tasks123.java
+++ src/com/company/ppj08/Tasks123.java
@@ -1,15 +1,27 @@
 package com.company.ppj08;
 
+import java.util.Scanner;
+
 public class Tasks123 {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
+        //task1();
+        //task2();
+        //task2_variant2();
+        //task3();
+        //task3_example2();
     }
 
     private static void task1() {
         for (int i = 0; i < 10; i++) {
             System.out.println(i + 1);
+        }
+    }
+
+    private static void task2_variant2() {
+        int result = 1;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(result);
+            result = result * 2;
         }
     }
 
@@ -22,13 +34,21 @@ public class Tasks123 {
 
     private static void task3() {
         int i = 5;
+        while (i < 5) {
+            System.out.println(i++);
+        }
+        i = 5;
         do {
-            System.out.println(i);
-        }
-        while (i != 5);
+            System.out.println(i++);
+        } while (i < 5);
+    }
 
-        while (i != 5) {
-            System.out.println(i);
-        }
+    private static void task3_example2() {
+        int answer;
+        do {
+            System.out.println("What is 2+2?");
+            Scanner scan = new Scanner(System.in);
+            answer = scan.nextInt();
+        } while (answer != 4);
     }
 }
