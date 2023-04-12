5707cf30557298773a9028d4fcbf2bfb57e5206c
Valeria <valtpocketbook@gmail.com>
2023-04-12 21:18:03 +0200

    ppj11 task91417 refactoring

--- src/com/company/M4/Tasks91417.java
+++ src/com/company/M4/Tasks91417.java
@@ -1,15 +1,25 @@
 package com.company.M4;
 
 public class Tasks91417 {
+
+    public static final int PRODUCT_INDEX = 0;
+    public static final int PRICE_INDEX = 1;
+    public static final String SEPARATOR = " ";
+
     public static void main(String[] args) {
         //task 9
-        System.out.println("Result with only stocks, which price is less than 200: " + getCheapStocks(new String[]{"gun 500", "firebow 70", "sunscreen 30", "pixboom 200"}));
+        //System.out.println("Result with only stocks, which price is less than 200: " + getCheapStocks(new String[]{"gun 500", "firebow 70", "sunscreen 30", "pixboom 200"}));
         //task 14
-        drawTriangle(4);
-        drawTriangle(3);
+        //drawTriangle(4);
+        //drawTriangle(3);
         //task 17
         System.out.println("Number of banknotes: " + countBanknotes(500));
         System.out.println("Number of banknotes: " + countBanknotes(578));
+        System.out.println("Number of banknotes: " + countBanknotes(1078));
+        System.out.println("Number of banknotes: " + countBanknotes(22));
+        System.out.println("Number of banknotes: " + countBanknotes(105));
+        System.out.println("Number of banknotes: " + countBanknotes(199));
+        System.out.println("Number of banknotes: " + countBanknotes(191));
     }
 
     public static String getCheapStocks(String[] stocks) {
@@ -21,13 +31,10 @@ public class Tasks91417 {
     }
 
     public static String checkIfLessThan200(String stock) {
-        String[] parts = stock.split(" ");
-        String nameStock = parts[0];
-        int priceStock = Integer.parseInt(parts[1]);
-        if (priceStock < 200) {
-            return nameStock + " ";
-        }
-        return "";
+        String[] tokens = stock.split(SEPARATOR);
+        String productName = tokens[PRODUCT_INDEX];
+        int price = Integer.parseInt(tokens[PRICE_INDEX]);
+        return (price < 200)?  productName + SEPARATOR : "";
     }
 
     public static void drawTriangle(int side) {
@@ -40,34 +47,11 @@ public class Tasks91417 {
 
     public static int countBanknotes(int sum) {
         int count = 0;
-        while (sum > 0) {
-            if (500 <= sum) {
-                sum -= 500;
-                count++;
-            } else if (200 <= sum) {
-                sum -= 200;
-                count++;
-            } else if (100 <= sum) {
-                sum -= 100;
-                count++;
-            } else if (50 <= sum) {
-                sum -= 50;
-                count++;
-            } else if (20 <= sum) {
-                sum -= 20;
-                count++;
-            } else if (10 <= sum) {
-                sum -= 10;
-                count++;
-            } else if (5 <= sum) {
-                sum -= 5;
-                count++;
-            } else if (2 <= sum) {
-                sum -= 2;
-                count++;
-            } else if (1 <= sum) {
-                sum -= 1;
-                count++;
+        int[] banknotes = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+        for (int i = 0; i < banknotes.length; i++) {
+            if ((sum / banknotes[i]) > 0) {
+                count += sum / banknotes[i];
+                sum %= banknotes[i];
             }
         }
         return count;
