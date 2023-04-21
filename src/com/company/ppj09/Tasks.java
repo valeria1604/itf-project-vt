15a6dd689bb49400cc6c77303fe1d5422e8a5b03
Valeria <valtpocketbook@gmail.com>
2023-04-21 22:18:11 +0200

    ppj09 last changes

--- src/com/company/ppj09/Tasks.java
+++ src/com/company/ppj09/Tasks.java
@@ -7,50 +7,48 @@ public class Tasks {
         //task01();
         //task02();
         //task03();
-        //task04();
+        task04();
     }
 
     private static void task01() {
-        for (int i = 75; i < 150; i++) {
-            System.out.println((char) i + " - " + i);
+        for (char i = 75; i < 150; i++) {
+            System.out.println(i + " - " + (int) i);
         }
     }
 
     private static void task02() {
+        double result = 1;
+        double sum = 0;
         for (int i = 1; i <= 10; i++) {
-            System.out.println(1 / Math.pow(2, i));
+            result = result / 2;
+            System.out.println(result);
+            sum = sum + result;
         }
+        System.out.println("Sum: " + sum);
     }
 
     private static void task03() {
-        for (int i = 1; i <= 15; i++) {
-            convertByteToHexAndSaveAsChar((byte) i);
+        for (byte i = 0; i <= 15; i++) {
+            String hexValue = Integer.toHexString(i);
+            char charValue = hexValue.charAt(0);
+            System.out.println(charValue);
         }
-    }
-
-    private static void convertByteToHexAndSaveAsChar(byte a) {
-        String hexadecimalValue = String.format("%02x", a);
-        char charValue = (char) Integer.parseInt(hexadecimalValue, 16);
-        System.out.println(String.valueOf(charValue));
     }
 
     private static void task04() {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         int widthAndHeight = 2 * size + 1;
-        int x = 1;
+
         for (int rows = 1; rows <= widthAndHeight; rows++) {
             for (int columns = 1; columns <= widthAndHeight; columns++) {
-                if ((columns <= (widthAndHeight / 2 + 1) - x) || (columns >= (widthAndHeight / 2 + 1) + x)) {
+                if ((columns + rows <= widthAndHeight / 2 + 1)
+                        || (columns + rows >= widthAndHeight + 1 + (widthAndHeight / 2 + 1))
+                        || (Math.abs(columns - rows) >= widthAndHeight / 2 + 1)) {
                     System.out.print(".");
                 } else {
                     System.out.print("x");
                 }
-            }
-            if (rows < widthAndHeight / 2 + 1) {
-                x++;
-            } else {
-                x--;
             }
             System.out.println();
         }
