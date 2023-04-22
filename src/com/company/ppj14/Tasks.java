dfcc591a8536121911050be0c564756f60fe823e
Valeria <valtpocketbook@gmail.com>
2023-04-22 11:02:41 +0200

    ppj14 fixed tasks 1-4

--- src/com/company/ppj14/Tasks.java
+++ src/com/company/ppj14/Tasks.java
@@ -5,27 +5,11 @@ public class Tasks {
         //task1();
         //task2();
         //task3();
-        //task4();
+        task4();
     }
 
     private static void task1() {
         System.out.println(powerValue(2, 5));
-    }
-
-    private static void task2() {
-        int value = 4;
-        System.out.println(sumBetweenZeroNumber(value - 1));
-    }
-
-    private static void task3() {
-        System.out.println(findCommonDivisor(24, 36));
-    }
-
-    public static void task4() {
-        int[] array = {1, 2, 3, 3, 2, 1};
-        System.out.println(checkIsItPalindrome(array));
-        int[] array2 = {1, 2, 3, 4, 5, 6};
-        System.out.println(checkIsItPalindrome(array2));
     }
 
     public static int powerValue(int number, int power) {
@@ -35,11 +19,20 @@ public class Tasks {
         return number * powerValue(number, power - 1);
     }
 
-    public static int sumBetweenZeroNumber(int value) {
+    private static void task2() {
+        int value = 4;
+        System.out.println(sumFromZeroToNumber(value - 1));
+    }
+
+    public static int sumFromZeroToNumber(int value) {
         if (value == 0) {
             return 0;
         }
-        return value + sumBetweenZeroNumber(value - 1);
+        return value + sumFromZeroToNumber(value - 1);
+    }
+
+    private static void task3() {
+        System.out.println(findCommonDivisor(24, 36));
     }
 
     public static int findCommonDivisor(int firstNumber, int secondNumber) {
@@ -50,21 +43,29 @@ public class Tasks {
         }
     }
 
-    public static boolean checkIsItPalindrome(int[] array) {
-        int startIndex = 0;
-        int lastIndex = array.length - 1;
-        return checkAreElementsEqual(startIndex, lastIndex, array);
+    public static void task4() {
+        int[] array = {1, 2, 3, 3, 2, 1};
+        System.out.println(checkIsItPalindrome(array));
+        int[] array2 = {1, 2, 3, 4, 5, 6};
+        System.out.println(checkIsItPalindrome(array2));
+        int[] array3 = {1, 2, 1};
+        System.out.println(checkIsItPalindrome(array3));
+        int[] array4 = {1, 2, 3, 4, 2, 1};
+        System.out.println(checkIsItPalindrome(array4));
+        int[] array5 = {1};
+        System.out.println(checkIsItPalindrome(array5));
     }
 
-    public static boolean checkAreElementsEqual(int firstIndex, int secondIndex, int[] array) {
-        if (array[firstIndex] != array[secondIndex]) {
-            return false;
-        } else {
-            if (array.length / 2 - 1 == firstIndex) {
-                return array[firstIndex] == array[secondIndex];
-            } else {
-                return checkAreElementsEqual(firstIndex + 1, secondIndex - 1, array);
-            }
+    public static boolean checkIsItPalindrome(int[] array) {
+        int startIndex = 0;
+        return checkAreElementsEqual(startIndex, array);
+    }
+
+    public static boolean checkAreElementsEqual(int firstIndex, int[] array) {
+        int secondIndex = array.length - 1 - firstIndex;
+        if (firstIndex == secondIndex || array.length / 2 - 1 == firstIndex) {
+            return array[firstIndex] == array[secondIndex];
         }
+        return checkAreElementsEqual(firstIndex + 1, array);
     }
 }
