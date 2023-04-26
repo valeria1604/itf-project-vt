f4bc5bc838e49acff1a6011455f504293afe9855
Valeria <valtpocketbook@gmail.com>
2023-04-26 19:54:11 +0200

    ppj14 task5 temporary

--- src/com/company/ppj14/Task5.java
+++ src/com/company/ppj14/Task5.java
@@ -5,10 +5,33 @@ public class Task5 {
         int rows = 10;
         int columns = rows;
         int[][] array = new int[rows][columns];
-        firstHalf(0, rows / 2, columns, array);
-        secondHalf((rows / 2), rows, columns, array);
-        printArray(rows, columns, array);
+        //printCol(3, 1, columns);
+        printCol(1, 1, columns);
+//        firstHalf(0, rows / 2, columns, array);
+//        secondHalf((rows / 2), rows, columns, array);
+//        printArray(rows, columns, array);
     }
+
+    //printRow (row, col, size) {
+    // printCol()
+    //printRow()
+    // printCol()
+    // }
+    private static void printCol(int max, int col, int size) {
+        if (col > size/2) {
+            return;
+        }
+        int value = col<max ? col: max;
+        System.out.print(value + " ");
+        printCol(max, col + 1, size);
+        System.out.print(value + " ");
+    }
+//printCol(5, 1, 10); it should print
+//"1234554321",
+//    printCol(4, 1, 10); it should print
+//"1234444321",
+//    printCol(3, 1, 10); it should print
+//"1233333321"
 
     private static void printArray(int rows, int columns, int[][] array) {
         for (int i = 0; i < rows; i++) {
@@ -42,7 +65,7 @@ public class Task5 {
         int number = 1;
         for (int i = 0; i < columns; i++) {
             array[row][i] = number;
-            if (number <= (row+1)/2+1  && i < columns - row) {
+            if (number <= (row + 1) / 2 + 1 && i < columns - row) {
                 number++;
             } else if (i > columns - row - 1) {
                 number--;
