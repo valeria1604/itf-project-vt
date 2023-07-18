8a7c71369bbd6b25ae975a63ec856f3c5e230b9a
Valeria <valtpocketbook@gmail.com>
2023-07-18 15:23:51 +0200

    M9 HashMap fixed

--- src/com/company/M9/Main.java
+++ src/com/company/M9/Main.java
@@ -6,7 +6,7 @@ public class Main {
         //testLinkedList();
         //testQueue();
         //testStack();
-        testHashMap();
+        //testHashMap();
 
     }
 
--- src/com/company/M9/MyHashMap.java
+++ src/com/company/M9/MyHashMap.java
@@ -1,105 +1,112 @@
 package com.company.M9;
 
-public class MyHashMap<K, V> {
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-    public MyHashMap.Node currentLastNode;
+public class MyHashMap<K, V> {
+    private class Element<K, V> {
+        private K key;
+        private V value;
+
+        private Element(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Element{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+    private ArrayList<LinkedList<Element<K, V>>> buckets;
+    private int numBuckets;
     private int size;
 
-    public void put(K key, V newValue) {
+    public MyHashMap() {
+        numBuckets = 10;
+        size = 0;
+        buckets = new ArrayList<>(numBuckets);
 
-        MyHashMap.Node currentCheckNode = currentLastNode;
-        for (int i = 0; i < size; i++) {
-            if (key == currentCheckNode.key) {
-                throw new IndexOutOfBoundsException("Key was already used");
+        for (int i = 0; i < numBuckets; i++) {
+            buckets.add(new LinkedList<>());
+        }
+    }
+
+    private int getHash(K key){
+        int hash = key.hashCode();
+        return hash % numBuckets;
+    }
+
+    public void put(K key, V value) {
+        int hash = getHash(key);
+        LinkedList<Element<K, V>> bucket = buckets.get(hash);
+
+        for (Element<K, V> element : bucket) {
+            if (element.getKey().equals(key)) {
+                element.value = value;
+                return;
             }
-            currentCheckNode = currentCheckNode.preNode;
-
         }
-        if (size == 0) {
-            MyHashMap.Node newNode = new MyHashMap.Node(key, newValue, null);
-            currentLastNode = newNode;
-        } else {
-            MyHashMap.Node newNode = new MyHashMap.Node(key, newValue, currentLastNode);
-            currentLastNode = newNode;
-        }
-
         size++;
+        bucket.add(new Element<>(key, value));
+    }
+
+    public V get(K key) {
+        int hash = getHash(key);
+        LinkedList<Element<K, V>> bucket = buckets.get(hash);
+        for (Element<K, V> element : bucket) {
+            if (element.getKey().equals(key)) {
+                return element.getValue();
+            }
+        }
+        return null;
     }
 
     public void remove(K key) {
-
-        MyHashMap.Node currentCheckNode = currentLastNode;
-
-        if (currentCheckNode.key == key) {
-            currentLastNode = currentCheckNode.preNode;
-        } else {
-            for (int i = 0; i < size; i++) {
-
-                if (key == currentCheckNode.preNode.key) {
-                    currentCheckNode.setPreNode(currentCheckNode.preNode.preNode);
-                    size--;
-                    return;
-                }
-                currentCheckNode = currentCheckNode.preNode;
+        int hash = getHash(key);
+        LinkedList<Element<K, V>> bucket = buckets.get(hash);
+        for (Element<K, V> element : bucket) {
+            if (element.getKey().equals(key)) {
+                bucket.remove(element);
+                size--;
             }
-
-            throw new IndexOutOfBoundsException("There is no element with that key");
+            break;
         }
+         throw new IndexOutOfBoundsException("There is no element with that key");
     }
 
     public void clear() {
+        numBuckets = 10;
         size = 0;
-        currentLastNode = null;
+        buckets = new ArrayList<>(numBuckets);
+
+        for (int i = 0; i < numBuckets; i++) {
+            buckets.add(new LinkedList<>());
+        }
     }
 
     public int size() {
         return size;
     }
 
-    public V get(K key) {
-
-        MyHashMap.Node currentCheckNode = currentLastNode;
-        for (int i = 0; i < size; i++) {
-            if (key == currentCheckNode.key) {
-                return (V) currentCheckNode.value;
-            }
-            currentCheckNode = currentCheckNode.preNode;
-        }
-
-        return null;
-    }
-
     @Override
     public String toString() {
-        if(size == 0){
-            return "Empty list";
-        }else{
-            String result = "[";
-            MyHashMap.Node tmpNode = currentLastNode;
-            result += "{" + tmpNode.value + "}, ";
-
-            while (tmpNode.preNode != null){
-                tmpNode = tmpNode.preNode;
-                result += "{" + tmpNode.value + "}, ";
-            }
-            result += "] \nSize: " + size;
-            return result;
-        }
-    }
-
-    private class Node<K, V> {
-        private K key;
-        private V value;
-        private Node<K, V> preNode;
-
-        private Node(K key, V value, Node<K, V> preNode) {
-            this.key = key;
-            this.value = value;
-            this.preNode = preNode;
-        }
-
-        public void setPreNode(Node preNode) {
-            this.preNode = preNode;
-        }
+        return "MyHashMap{" +
+                "buckets=" + buckets +
+                ", numBuckets=" + numBuckets +
+                ", size=" + size +
+                '}';
     }
 }
