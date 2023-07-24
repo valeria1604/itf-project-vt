f128ffb9cdab80ecb76fba4572950c2de9d0fcc2
Valeria <valtpocketbook@gmail.com>
2023-07-25 00:56:09 +0200

    M10 fixed

--- src/com/company/M10/Task2.java
+++ src/com/company/M10/Task2.java
@@ -1,10 +1,19 @@
 package com.company.M10;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task2 {
+
+    public static final String SPACE = " ";
+    public static final int NAME_PLACE = 0;
+    public static final int AGE_PLACE = 1;
+    public static final int TWO_VALUES = 2;
+
     public static void main(String[] args) {
         String inputFileName = "file.txt";
         String outputFileName = "user.json";
@@ -17,10 +26,10 @@ public class Task2 {
         List<User> users = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
             String line = reader.readLine();
-            while ((line = reader.readLine()) != null){
-                String[] data = line.split(" ");
-                if(data.length == 2){
-                    users.add(new User(data[0], Integer.parseInt(data[1])));
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(SPACE);
+                if (data.length == TWO_VALUES) {
+                    users.add(new User(data[NAME_PLACE], Integer.parseInt(data[AGE_PLACE])));
                 }
             }
         } catch (IOException e) {
@@ -31,10 +40,8 @@ public class Task2 {
 
     private static void writeToFile(String outputFileName, List<User> users) {
 //        Gson gson = new Gson();
-//        String json = gson.toJson(users);
-//        System.out.println(json);
-//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
-//            writer.write(json);
+//        try {
+//            gson.toJson(users, new FileWriter(outputFileName));
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
--- src/com/company/M10/Task3.java
+++ src/com/company/M10/Task3.java
@@ -1,40 +1,33 @@
 package com.company.M10;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Task3 {
     public static void main(String[] args) {
-//        String sentence = "the day is sunny the the\n" +
-//                "the sunny is is";
-//        Map<String, Integer> words = new HashMap<>();
-//        calculateRepetitions(words, sentence);
-
-//        String inputFileName = "file.txt";
-//        Map<String, Integer> words = new HashMap<>();
-//        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                calculateRepetitions(words, line);
-//            }
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//
-//        System.out.println(words);
+        String inputFileName = "file.txt";
+        Map<String, Integer> words = calculateWordFrequencies(inputFileName);
+        System.out.println(words);
     }
 
-    private static void calculateRepetitions(Map<String, Integer> words, String line) {
-        String[] data = line.split("\\s+");
-        System.out.println(Arrays.toString(data));
-        for (String word : data) {
-            if (!words.containsKey(word)) {
-                words.put(word, 1);
-            } else {
-                int currentFrequency = words.getOrDefault(word, 0);
-                words.put(word, currentFrequency + 1);
+    private static Map<String, Integer> calculateWordFrequencies(String inputFileName) {
+        Map<String, Integer> words = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split("\\s+");
+                for (String word : data) {
+                    int currentFrequency = words.getOrDefault(word, 0);
+                    words.put(word, currentFrequency + 1);
+                }
+                System.out.println(words);
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        System.out.println(words);
+        return words;
     }
 }
