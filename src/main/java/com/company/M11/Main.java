20078d61867ca833cd200112d84d935dd8f887cb
Valeria <valtpocketbook@gmail.com>
2023-09-13 01:27:52 +0200

    11 Module fixes

--- src/main/java/com/company/M11/Main.java
+++ src/main/java/com/company/M11/Main.java
@@ -2,7 +2,6 @@ package com.company.M11;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -18,40 +17,48 @@ public class Main {
     private static void task1() {
         List<String> names = Arrays.asList("John", "Bill", "Anthon", "Alex");
 
-        AtomicInteger index = new AtomicInteger(0);
-        Stream<String> namesStream = names.stream().filter(name -> index.getAndIncrement() % 2 != 0);
-        namesStream.forEachOrdered(name -> System.out.println(index.get() - 1 + ". " + name));
+        IntStream.range(0, names.size())
+                .filter(index -> index % 2 != 0)
+                .forEachOrdered(index -> System.out.println(index + ". " + names.get(index)));
     }
 
     private static void task2() {
         List<String> names = Arrays.asList("John", "Bill", "Anthon", "Alex");
-        Stream<String> namesStream = names.stream().sorted().map(String::toUpperCase);
-        namesStream.forEachOrdered(System.out::println);
+        names.stream()
+                .sorted()
+                .map(String::toUpperCase)
+                .forEachOrdered(System.out::println);
     }
 
     private static void task3() {
-        int[] numbers = {1, 2, 0, 4, 5};
-        IntStream numbersStream = Arrays.stream(numbers).sorted();
-        numbersStream.forEach(number -> System.out.print(number + ", "));
+        String[] inputArray = {"1, 2, 0", "4, 5"};
+        int[] numbers = Arrays.stream(inputArray)
+                .flatMapToInt(s -> Arrays.stream(s.split(", "))
+                        .mapToInt(Integer::parseInt))
+                .toArray();
+        Arrays.stream(numbers).
+                sorted()
+                .forEach(number -> System.out.print(number + ", "));
     }
 
     private static void task4() {
         long a = 25214903917L;
         long c = 11;
         long m = (long) Math.pow(2, 48);
-        Stream<Long> randomStream = generateStream(1L, a, c, m, 10);
-        randomStream.forEach(System.out::println);
+        generateStream(1L, a, c, m, 10)
+                .forEach(System.out::println);
     }
 
     private static Stream<Long> generateStream(long seed, long a, long c, long m, int limit) {
-        return Stream.iterate(seed, x -> (a * x + c) % m).limit(limit);
+        return Stream.iterate(seed, x -> (a * x + c) % m)
+                .limit(limit);
     }
 
     private static void task5() {
         Stream<Integer> stream1 = Stream.of(1, 3, 5, 7, 9);
         Stream<Integer> stream2 = Stream.of(2, 4, 6);
-        Stream<Integer> zippedStream = zipStream(stream1, stream2);
-        zippedStream.forEach(System.out::println);
+        zipStream(stream1, stream2)
+                .forEach(System.out::println);
     }
 
     private static <T> Stream<T> zipStream(Stream<T> stream1, Stream<T> stream2) {
